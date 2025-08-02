ae892d105371a48a1af2dd83d4e156cebe2daa4c
Joseph Shi <joseph.shi@live.cn>
2025-08-02 21:55:42 +0800

    re-order case

---
+++ cypress/e2e/00_go_to_ws.cy.js
@@ -0,0 +1,22 @@
+describe('go to workspace page', () => {
+  it('navigate to workspace page', () => {
+    cy.allure().startStep('Open Kong UI');
+    cy.visit('/workspaces')
+    cy.get('[data-testid="sidebar-item-workspaces"]')
+    .should('have.class', 'sidebar-item-primary')
+    .and('have.class', 'active');
+    cy.allure().endStep();
+    cy.allure().feature('Service Management workspace');
+    cy.allure().severity('critical');
+  });
+  it('find default workspace', () => {
+    cy.visit('/workspaces')
+    cy.get('.workspace-name').then($el => {
+      const text = $el.text().trim();
+      if (text.includes('default')) {
+        cy.wrap($el).click();
+      } else {
+        throw new Error(`element workspace-name does not contain default, but is：${text}`);
+      }
+    });
+  });
--- cypress/e2e/01_create_server_and_route.cy.js
+++ cypress/e2e/01_create_server_and_route.cy.js
@@ -8,13 +8,13 @@ describe('create service and route', () => {
     cy.get('[data-testid="action-button"]').click();
     cy.get('[data-testid="gateway-service-url-radio-label"] > .card-label-container > .radio-label').click();
     cy.get('[data-testid="gateway-service-url-radio"]').check();
-    cy.get('[data-testid="gateway-service-url-input"]').type('https://localhost:8002/test');
+    cy.get('[data-testid="gateway-service-url-input"]').type('http://httpbin.konghq.com');
     cy.get('[data-testid="gateway-service-name-input"]').clear('n');
-    cy.get('[data-testid="gateway-service-name-input"]').type('new-service-for-testing');
+    cy.get('[data-testid="gateway-service-name-input"]').type('test_service');
     cy.get('[data-testid="service-create-form-submit"]').click();
     cy.get('.alert-message > .k-button').click();
     /* ==== Generate route ==== */
-    cy.get('[data-testid="route-form-name"]').type('route_1');
+    cy.get('[data-testid="route-form-name"]').type('test_route');
     cy.get('[data-testid="route-form-paths-input-1"]').type('/api/v1');
     cy.get('.expanded-selection-empty').click();
     cy.get('[data-testid="multiselect-item-GET"] > .multiselect-item-container > button > .multiselect-item-label').click();
@@ -30,15 +30,15 @@ describe('create service and route', () => {
     cy.get('[data-testid="row-actions-dropdown-trigger"]').click();
     cy.get('[data-testid="action-entity-delete"] > .dropdown-item-trigger-label').click();
     cy.get('[data-testid="confirmation-input"]').clear('r');
-    cy.get('[data-testid="confirmation-input"]').type('route_1');
+    cy.get('[data-testid="confirmation-input"]').type('test_route');
     cy.get('[data-testid="modal-action-button"]').click();
     /* ==== remove service ==== */
     cy.get('[data-testid="sidebar-item-gateway-services"] > .sidebar-item-link > .sidebar-item-display > .sidebar-item-name-container > .sidebar-item-name').click();
     cy.get('[data-testid="row-actions-dropdown-trigger"]').click();
     cy.get('[data-testid="action-entity-delete"] > .dropdown-item-trigger-label').click();
     cy.get('.confirmation-text').click();
-    cy.get('[data-testid="confirmation-input"]').clear('new-service-for-testing');
-    cy.get('[data-testid="confirmation-input"]').type('new-service-for-testing');
+    cy.get('[data-testid="confirmation-input"]').clear('test_service');
+    cy.get('[data-testid="confirmation-input"]').type('test_service');
     cy.get('[data-testid="modal-action-button"]').click();
   })
 })
