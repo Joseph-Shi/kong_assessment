e8fda3f2695e06d9f2cc62633855713549e7f694
Joseph Shi <joseph.shi@live.cn>
2025-08-03 15:57:25 +0800

    fix case issue

--- cypress/testcases/case_create_service.js
+++ cypress/testcases/case_create_service.js
@@ -7,14 +7,5 @@ export const cases = [
     routeName: "routeA",
     routePath: "/api/v1",
     methods: ["GET", "PUT", "POST", "PATCH", "DELETE"]
-  },
-  {
-    name: "create service B and routeB",
-    service_name: "svcB",
-    serviceUrl: "http://httpbin.konghq.com",
-    serviceUrl_validate: "httpbin.konghq.com",
-    routeName: "routeB",
-    routePath: "/api/v1",
-    methods: ["GET", "PUT", "POST", "PATCH", "DELETE"]
   }
 ];
